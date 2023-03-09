5b638c8c95175a858a07454e4bf64b698267ae0e
kim-seonkyu <seonkyu93@gmail.com>
2023-03-10 00:03:45 +0900

    searchpage / app(router)&globalstyle&theme / footer / booksearchform

---
+++ src/components/Theme.js
@@ -0,0 +1,9 @@
+export const themeLight = {
+  text: "#000",
+  backgroundColor: "#fff",
+};
+
+export const themeDark = {
+  text: "#fff",
+  backgroundColor: "#121212",
+};
---
+++ src/pages/SearchPage.js
@@ -0,0 +1,48 @@
+import styled from "@emotion/styled";
+import React from "react";
+import BookSearchForm from "../components/BookSearchForm";
+
+const LogoText = styled.h3`
+  margin: 0;
+`;
+
+const Container = styled.div`
+  max-width: 960px;
+  padding: 15px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  border-bottom: 1px solid gray;
+`;
+
+const HeaderContainer = styled(Container)`
+  display: flex;
+  align-items: center;
+
+  @media (max-width: 778px) {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+`;
+
+const HeaderSearchForm = styled.div`
+  margin-left: auto;
+`;
+
+const SearchPage = () => {
+  return (
+    <>
+      <Header>
+        <HeaderContainer>
+          <LogoText>Book List</LogoText>
+          <HeaderSearchForm>
+            <BookSearchForm />
+          </HeaderSearchForm>
+        </HeaderContainer>
+      </Header>
+    </>
+  );
+};
+
+export default SearchPage;
